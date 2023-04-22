0f81552e5427125366cd52bbc31613392befb926
Mohan Kumar <nohamkumar@yahoo.com>
2023-04-22 20:09:09 +0530

    Add: iam_role_name output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
-output "role_arn" {
-  value = aws_iam_role.role.arn
+output "iam_role_arn" {
+  description = "AWS IAM role ARN"
+  value       = aws_iam_role.role.arn
+}
+
+output "iam_role_name" {
+  description = "AWS IAM role name"
+  value       = aws_iam_role.role.name
 }
