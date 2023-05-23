02a7c724febcc4a6c73937c1bb70f0de47a4fe11
bigfantech <130840188+bigfantech@users.noreply.github.com>
2023-05-23 18:20:17 +0530

    Add: k8s_service_account_name output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "iam_role_name" {
   description = "AWS IAM role name"
   value       = aws_iam_role.role.name
 }
+
+output "k8s_service_account_name" {
+  description = "Name of the Kuberentes ServiceAccount created"
+  value       = var.service_account_name
+}
