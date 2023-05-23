60fad28320cc99ab6c2b42a89405ab6cb107ebc0
bigfantech <130840188+bigfantech@users.noreply.github.com>
2023-05-23 17:59:32 +0530

    change: var name create_service_account

--- main.tf
+++ main.tf
@@ -91,7 +91,7 @@ resource "aws_iam_role_policy_attachment" "policy_arns_list" {
 #---
 
 resource "kubernetes_service_account" "service_account" {
-  count = var.create_serviceaccount ? 1 : 0
+  count = var.create_service_account ? 1 : 0
 
   metadata {
     name      = var.service_account_name
